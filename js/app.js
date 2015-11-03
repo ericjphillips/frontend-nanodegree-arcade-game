35c11ee15d6203b6a98c95768626d3e2bf625a6c
ericjphillips <ericj.phillips@gmail.com>
2015-11-03 15:21:40 -0500

    define all variables and draw player and enemy on canvas

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 200;
+    this.y = 200;
 };
 
 // Update the enemy's position, required method for game
@@ -24,11 +26,31 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/char-boy.png'
+    this.x = 100;
+    this.y = 100;
+}
 
+Player.prototype.update = function(dt) {
+
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.handleInput = function() {
+
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var firstEnemy = new Enemy;
+var allEnemies = [firstEnemy];
+
+var player = new Player;
 
 
 
